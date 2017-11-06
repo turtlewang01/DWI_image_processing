40093ee778c8ede23ffabd08a2900408f085425c
turtlewang01 <33196347+turtlewang01@users.noreply.github.com>
2017-11-06 21:46:29 +0800

    add ROI function
    two points:
    1. add ROI function to the main .m file;
    2. add PENGXIAOYAN'S ROI region data file;

--- main_ver4.m
+++ main_ver4.m
@@ -2,9 +2,12 @@ clc
 clear
 % file_path='D:\part_time_job\DWI\IVIM1\IVIM1\IM';
 file_path='D:\part_time_job\DWI\IVIM1\PENG_XIAOYAN\IM';
-solve_method=4; %1=Biexp, 2=LS,3=Mix,4= fix D_star?5=sove 3 variable simultaneously
+solve_method=1; %1=Biexp, 2=LS,3=Mix,4= fix D_star?5=sove 3 variable simultaneously
+d_method=1; % 1=use ADC as d,2=use LS method fitting
 data_source='DICOM'; % DICOM or nii;
-
+opti_method='trust-region-reflective'; % trust-region-reflective method or levenberg-marquardt method
+ROI_create=0;% 1: create the ROI,0= load the ROI;
+options.Algorithm = opti_method;
 
 %% this section choose the DICOM image
 if(strcmp(data_source,'DICOM'))
@@ -63,12 +66,22 @@ end
 %     I(:,:,i)=imfilter(I(:,:,i),h);
 % end
 
-I=I(40:244,45:212,:);
+% I=I(40:244,45:212,:);
 h_handle=figure
 imagesc(I(:,:,1));
-title('Original Pic');
-% BW1 = edge(I(:,:,1),'Prewitt');
-% mask = createMask(BW1,I(:,:,1));
+title(strcat('Original Pic/',metadata.PatientName.FamilyName))
+
+%% create ROI or load ROI%%
+if(ROI_create)
+    BW = roipoly(I(:,:,1));
+    ROI_file=strcat('BW_',metadata.PatientName.FamilyName);
+    save(ROI_file,'BW');
+else
+    BW=load(strcat('BW_',metadata.PatientName.FamilyName));
+    BW=BW.BW;
+end
+
+
 
 
 I=double(I);
@@ -87,130 +100,157 @@ num_start=9;
 num_end=13;
 n_length=num_end-num_start+1;
 b_val_nonzero=b_val(1:end-1);
-options.Algorithm = 'trust-region-reflective';
-% options.Algorithm = 'levenberg-marquardt';
+
 
 
 for(i=1:m_row)
     for(j=1:n_col)
-        S0=I(i,j,14);
-        for(kk=1:14)
-            Sb(kk)=I(i,j,kk);
-        end
-        Sb_nonzero=Sb(1:13);
-        
-        if(max(Sb_nonzero)>S0)
-            Error_model_matrix(i,j)=1;
-        end
-      
-        
-        if(abs(S0)>0.3)
-            Sb_normalize=Sb/S0;
-        else
-            Sb_normalize=zeros(1,14);
-        end
-        b_comp=b_val(num_start:num_end);
-        Sb_normalize_comp=Sb_normalize(num_start:num_end);
-        for(k=num_start:num_end)
-            Sb_comp(k-num_start+1)=I(i,j,k);
-        end
-        
-        switch solve_method
-            case 1
-                if(abs(min([Sb,S0]))<5)
-                    f_matrix(i,j)=0;
-                else
-                    D=-log(Sb_comp(end)/S0)/b_comp(end);
-                    if(D<0)
-                        D=0;
-                        Error_D_matrix(i,j)=1;
-                    end
-                    D_matrix(i,j)=D;
-                    if(D==0)
-%                         f_matrix(i,j)=1-Sb_comp(end)/S0;
+        if(BW(i,j)==1)
+            S0=I(i,j,14);
+            for(kk=1:14)
+                Sb(kk)=I(i,j,kk);
+            end
+            Sb_nonzero=Sb(1:13);
+            
+            if(max(Sb_nonzero)>S0)
+                Error_model_matrix(i,j)=1;
+            end
+            
+            
+            if(abs(S0)>0.3)
+                Sb_normalize=Sb/S0;
+            else
+                Sb_normalize=zeros(1,14);
+            end
+            b_comp=b_val(num_start:num_end);
+            Sb_normalize_comp=Sb_normalize(num_start:num_end);
+            for(k=num_start:num_end)
+                Sb_comp(k-num_start+1)=I(i,j,k);
+            end
+            
+            switch solve_method
+                case 1
+                    if(abs(min([Sb,S0]))<5)
                         f_matrix(i,j)=0;
                     else
-                        fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-D.*b_val))-Sb_normalize;
-                        x0 = [0.0025,25*D];
-                        lb=[0,1.5*D];
-                        ub=[0.05,0.3];
-                        [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
-%                         [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
-                        f_matrix(i,j)=x(1);
-                        D_star_matrix(i,j)=x(2);
+                        switch d_method
+                            case 1
+                                D=-log(Sb_comp(end)/S0)/b_comp(end);
+                            case 2
+                                y_temp=log(Sb_comp/S0)';
+                                A_temp=-b_comp';
+                                D=pinv(A_temp)*y_temp;
+                            otherwise
+                                disp('under construction!');
+                        end
+                        
+                        if(D<0)
+                            D=0;
+                            Error_D_matrix(i,j)=1;
+                        end
+                        D_matrix(i,j)=D;
+                        if(D==0)
+                            %                         f_matrix(i,j)=1-Sb_comp(end)/S0;
+                            f_matrix(i,j)=0;
+                        else
+                            fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-D.*b_val))-Sb_normalize;
+                            x0 = [0.0025,25*D];
+                            lb=[0,1.5*D];
+                            ub=[0.3,0.3];
+                            if(strcmp(opti_method,'trust-region-reflective'))
+                                [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
+                            else
+                                [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
+                            end
+                            
+                            f_matrix(i,j)=x(1);
+                            D_star_matrix(i,j)=x(2);
+                        end
                     end
-                end
-            case 2
-                if(abs(min([Sb,S0]))<1)
-                    f_matrix(i,j)=0;
-                    D_matrix(i,j)=0;
-                else
-                    delta=0;
-                    Sb_normalize_comp_log=log(Sb_normalize_comp)-delta;
-                    coef=[-b_comp',ones(num_end-num_start+1,1)];
-                    temp=pinv(coef)*Sb_normalize_comp_log';
-                    D_matrix(i,j)=temp(1);
-                    f_matrix(i,j)=1-exp(temp(2));
-                end
-            case 3
-                if(abs(min([Sb,S0]))<5)
-                    f_matrix(i,j)=0;
-                    D_matrix(i,j)=0;
-                else
-                    D=-log(Sb_comp(end)/S0)/b_comp(end);
-                    D_matrix(i,j)=D;
-                    if(D==0)
-                        f_matrix(i,j)=1-Sb_comp(end)/S0;
+                case 2
+                    if(abs(min([Sb,S0]))<1)
+                        f_matrix(i,j)=0;
+                        D_matrix(i,j)=0;
                     else
-                        Sb_normalize_comp_log=log(Sb_normalize_comp);
+                        delta=0;
+                        Sb_normalize_comp_log=log(Sb_normalize_comp)-delta;
                         coef=[-b_comp',ones(num_end-num_start+1,1)];
                         temp=pinv(coef)*Sb_normalize_comp_log';
                         D_matrix(i,j)=temp(1);
                         f_matrix(i,j)=1-exp(temp(2));
+                    end
+                case 3
+                    if(abs(min([Sb,S0]))<5)
+                        f_matrix(i,j)=0;
+                        D_matrix(i,j)=0;
+                    else
+                        D=-log(Sb_comp(end)/S0)/b_comp(end);
+                        D_matrix(i,j)=D;
+                        if(D==0)
+                            f_matrix(i,j)=1-Sb_comp(end)/S0;
+                        else
+                            Sb_normalize_comp_log=log(Sb_normalize_comp);
+                            coef=[-b_comp',ones(num_end-num_start+1,1)];
+                            temp=pinv(coef)*Sb_normalize_comp_log';
+                            D_matrix(i,j)=temp(1);
+                            f_matrix(i,j)=1-exp(temp(2));
+                            
+                            lb=[0,1.5*D];
+                            ub=[0.25,0.3];
+                            fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-D.*b_val))-Sb_normalize;
+                            x0 = [15*D_matrix(i,j),f_matrix(i,j)];
+                            
+                            if(strcmp(opti_method,'trust-region-reflective'))
+                                [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
+                            else
+                                [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
+                            end
+                            f_matrix(i,j)=x(1);
+                            D_star_matrix(i,j)=x(2);
+                        end
+                    end
+                case 4
+                    if(abs(min(Sb))<5)
+                        f_matrix(i,j)=0;
+                    else
+                        D_star_value=0.02;
+                        D_star_matrix(i,j)=D_star_value;
+                        fun = @(x)(x(1).*exp(-D_star_value.*b_val)+(1-x(1)).*exp(-x(2).*b_val))-Sb_normalize;
+                        x0 = [0.0025,D_star_value/25];
+                        lb=[0,0];
+                        ub=[0.05,D_star_value/1.5];
+                        if(strcmp(opti_method,'trust-region-reflective'))
+                            [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
+                        else
+                            [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
+                        end
                         
-                        lb=[0,1.5*D];
-                        ub=[0.25,0.3];
-                        fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-D.*b_val))-Sb_normalize;
-                        x0 = [15*D_matrix(i,j),f_matrix(i,j)];
-                        % [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
-                        [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun);
+                        f_matrix(i,j)=x(1);
+                        D_matrix(i,j)=x(2);
+                    end
+                case 5
+                    if(abs(min([Sb,S0]))<5)
+                        f_matrix(i,j)=0;
+                    else
+                        fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-x(3).*b_val))-Sb_normalize;
+                        x0 = [0.0025,0.005,0.005];
+                        lb=[0,0,0];
+                        ub=[0.05,0.3,0.05];
+                        if(strcmp(opti_method,'trust-region-reflective'))
+                            [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
+                        else
+                            [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
+                        end
+                        
                         f_matrix(i,j)=x(1);
                         D_star_matrix(i,j)=x(2);
+                        D_matrix(i,j)=x(3);
                     end
-                end
-            case 4
-                if(abs(min(Sb))<5)
-                    f_matrix(i,j)=0;
-                else
-                    D_star_value=0.02;
-                    D_star_matrix(i,j)=D_star_value;                    
-                    fun = @(x)(x(1).*exp(-D_star_value.*b_val)+(1-x(1)).*exp(-x(2).*b_val))-Sb_normalize;
-                    x0 = [0.0025,D_star_value/25];
-                    lb=[0,0];
-                    ub=[0.05,D_star_value/1.5];
-%                                         [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
-                    [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
-                    f_matrix(i,j)=x(1);
-                    D_matrix(i,j)=x(2);                   
-                end
-            case 5
-                if(abs(min([Sb,S0]))<5)
-                    f_matrix(i,j)=0;
-                else
-                    fun = @(x)(x(1).*exp(-x(2).*b_val)+(1-x(1)).*exp(-x(3).*b_val))-Sb_normalize;
-                    x0 = [0.0025,0.005,0.005];
-                    lb=[0,0,0];
-                    ub=[0.05,0.3,0.05];
-                    [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,lb,ub,options);
-                    %                         [x,resnorm,residual,exitflag,output,lambda,jacobian] = lsqnonlin(fun,x0,[],[],options);
-                    f_matrix(i,j)=x(1);
-                    D_star_matrix(i,j)=x(2);
-                    D_matrix(i,j)=x(3);                     
-                end
-            otherwise
-                warning('Unexpected plot type. No plot created.')
+                otherwise
+                    warning('Unexpected plot type. No plot created.')
+            end
         end
-    end    
+    end
 end
 
 
@@ -234,7 +274,7 @@ index_D=find(D_matrix<0);
 Dfigure_matrix(index_D)=0.0;
 imagesc(Dfigure_matrix);
 colorbar
-title(strcat('D map\',metadata.PatientName.FamilyName))
+title(strcat('D map/',metadata.PatientName.FamilyName))
 
 
 
